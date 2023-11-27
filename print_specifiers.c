486c6509584ed93168d38a1ac9c785c9f88d4a8b
MorrisCastro <jomarcastro2004@gmail.com>
2023-11-27 09:20:33 -0400

    add negative var

--- print_specifiers.c
+++ print_specifiers.c
@@ -73,10 +73,10 @@ int print_int(va_list args)
 {
 	int num = va_arg(args, int);
 
-    char buffer[20];
-    int length = 0;
-    int d, j;
-	int digit;
+	char buffer[20];
+	int length = 0;
+	int d, j;
+	int digit, negative = 0;
 
 	if (num < 0)
 	{
@@ -92,10 +92,11 @@ int print_int(va_list args)
 			digit *= -1;
 		}
 		buffer[length++] = '0' + digit;
-		num/=10;
-    } while (num != 0);
-	 d = (buffer[0] == '-') ? 1 : 0;	
-    j = length - 1;
+		num /= 10;
+	}while (num != 0);
+	
+	d = (buffer[0] == '-') ? 1 : 0;
+	j = length - 1;
 
 	while (d < j)
 	{
@@ -105,7 +106,7 @@ int print_int(va_list args)
 		buffer[j--] = temp;
 	}
 
-    write(1, buffer, length);
-
-    return length;
+	write(1, buffer, length);
+	length += negative;
+	return (length);
 }
