8a05c0ce6fff5b182a4ae6767800c65ce0ba44d0
Brook <christianlopezrivera0@gmail.com>
2023-11-26 23:33:10 -0400

    uploading the README file still trying to fix task 1

--- print_specifiers.c
+++ print_specifiers.c
@@ -93,7 +93,7 @@ int print_int(va_list args)
 		buffer[length++] = '0' + digit;
 		num/=10;
     } while (num != 0);
-	 d = (buffer[0] == '-') ? 1 : 0;	
+	 d = (buffer[1] == '-') ? 1 : 0;	
     j = length - 1;
 
     while (d < j) {
@@ -102,7 +102,7 @@ int print_int(va_list args)
         buffer[j--] = temp;
     }
 
-    write(0, buffer, length);
+    write(1, buffer, length);
 
     return length;
 }
